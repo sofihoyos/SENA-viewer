19c64f7aef53dca31d8e46e2f481a08144f168bc
sofihoyos <sofi.hoyos.2509@gmail.com>
2024-08-15 10:45:24 -0500

    Creacion de la clase methods e inicio del menu

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        int opcionMenu= -1;
+        while(opcionMenu != 0) {
+            Methods.mostrarMenu();
+            opcionMenu= scanner.nextInt();
+            switch (opcionMenu){
+                case 1:
+                    System.out.println("MOVIES");
+
+            }
+
+        }
     }
 }
---
+++ src/Methods.java
@@ -0,0 +1,13 @@
+public class Methods {
+    public static void mostrarMenu() {
+        System.out.println("Menu:");
+        System.out.println("1. Movies");
+        System.out.println("2. Series");
+        System.out.println("3. Books");
+        System.out.println("4. Magazines");
+        System.out.println("5. Report");
+        System.out.println("6. Report Today");
+        System.out.println("0. Exit");
+        System.out.print("Seleccione una opcion (0-6): ");
+    }
+}
