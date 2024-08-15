29a494f793b5f571ec7dee82d6bdf6d13990010a
sofihoyos <sofi.hoyos.2509@gmail.com>
2024-08-15 09:22:42 -0500

    Creacion de la clase Book con atributos,contructor y getters y setters

---
+++ src/edu/misena/senaviewer/model/Book.java
@@ -0,0 +1,92 @@
+package edu.misena.senaviewer.model;
+
+import java.util.Date;
+
+public class Book {
+    private int id;
+    private String title;
+    private Date editionDate;
+    private String editorial;
+    private String authors;
+    private String isbn;
+    private Boolean readed;
+    private int timeReaded;
+
+    public Book(
+            String title,
+            Date editionDate,
+            String editorial,
+            String isbn
+    ){
+        this.title=title;
+        this.editionDate=editionDate;
+        this.editorial=editorial;
+        this.isbn=isbn;
+    }
+
+    //getters//
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Date getEditionDate() {
+        return editionDate;
+    }
+
+    public String getEditorial() {
+        return editorial;
+    }
+
+    public String getAuthors() {
+        return authors;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public Boolean getReaded() {
+        return readed;
+    }
+
+    public int getTimeReaded() {
+        return timeReaded;
+    }
+
+    //setters
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setEditionDate(Date editionDate) {
+        this.editionDate = editionDate;
+    }
+
+    public void setEditorial(String editorial) {
+        this.editorial = editorial;
+    }
+
+    public void setAuthors(String authors) {
+        this.authors = authors;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public void setReaded(boolean readed) {
+        this.readed = readed;
+    }
+
+    public void setTimeReaded(int timeReaded) {
+        this.timeReaded = timeReaded;
+    }
+}
