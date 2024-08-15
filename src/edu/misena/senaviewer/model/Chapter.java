61af8a483990c838c972a5a021afb03dc2657dc3
sofihoyos <sofi.hoyos.2509@gmail.com>
2024-08-15 09:33:57 -0500

    Creacion de la clase Chapter con atributos,contructor y getters y setters

--- src/edu/misena/senaviewer/model/Chapter.java
+++ src/edu/misena/senaviewer/model/Chapter.java
@@ -1,7 +1,79 @@
 package edu.misena.senaviewer.model;
 
 public class Chapter {
-    public static void main(String[] args){
+    private int id;
+    private String title;
+    private int duration;
+    private int year;
+    private boolean viewed;
+    private int timeViewed;
+    private int sessionNumber;
 
+    public Chapter(
+            String title,
+            int duration,
+            int year
+    ){
+        this.title=title;
+        this.duration=duration;
+        this.year=year;
+    }
+
+    //getters//
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public boolean getViewed() {
+        return viewed;
+    }
+
+    public int getTimeViewed() {
+        return timeViewed;
+    }
+
+    public int getSessionNumber() {
+        return sessionNumber;
+    }
+
+    //setters
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void setViewed(boolean viewed) {
+        this.viewed = viewed;
+    }
+
+    public void setTimeViewed(int timeViewed) {
+        this.timeViewed = timeViewed;
+    }
+
+    public void setSessionNumber(int sessionNumber) {
+        this.sessionNumber = sessionNumber;
     }
 }
