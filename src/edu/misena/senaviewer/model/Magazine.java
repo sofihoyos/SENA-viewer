3c828e54f7f2f6d9d4cae8af634a350d14431d01
sofihoyos <sofi.hoyos.2509@gmail.com>
2024-08-15 10:19:01 -0500

    Creacion de la clase Magazine con atributos,contructor y getters y setters

--- src/edu/misena/senaviewer/model/Magazine.java
+++ src/edu/misena/senaviewer/model/Magazine.java
@@ -1,7 +1,63 @@
 package edu.misena.senaviewer.model;
 
-public class Magazine {
-    public static void main(String[] args){
+import java.util.Date;
 
+public class Magazine {
+    private int id;
+    private String title;
+    private Date editionDate;
+    private String editorial;
+    private String authors;
+
+    public Magazine(
+            String title,
+            Date editionDate,
+            String editorial
+    ){
+        this.title=title;
+        this.editionDate=editionDate;
+        this.editorial=editorial;
+    }
+
+    //getters//
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Date getEditionDate() {
+        return editionDate;
+    }
+
+    public String getEditorial() {
+        return editorial;
+    }
+
+    public String getAuthors() {
+        return authors;
+    }
+
+    //setters
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setEditionDate(Date editionDate) {
+        this.editionDate = editionDate;
+    }
+
+    public void setEditorial(String editorial) {
+        this.editorial = editorial;
+    }
+
+    public void setAuthors(String authors) {
+        this.authors = authors;
     }
 }
