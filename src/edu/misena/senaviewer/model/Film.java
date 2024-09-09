849b69b1fc5104107e11bfc485397c2d6f405822
sofihoyos <169062995+sofihoyos@users.noreply.github.com>
2024-09-09 07:03:42 -0500

    creacion de la clase film

--- src/edu/misena/senaviewer/model/Film.java
+++ src/edu/misena/senaviewer/model/Film.java
@@ -1,4 +1,67 @@
 package edu.misena.senaviewer.model;
 
 public class Film {
+    private int id;
+    private String title;
+    private String gender;
+    private String creator;
+    private int duration;
+    private int year;
+    private boolean viewed;
+
+    public Film(int id, String title, String gender, String creator, int duration, int year, boolean viewed) {
+        this.id = id;
+        this.title = title;
+        this.gender = gender;
+        this.creator = creator;
+        this.duration = duration;
+        this.year = year;
+        this.viewed = viewed;
+    }
+    
+    //getters
+    public int getId() {
+        return id;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public String getGender() {
+        return gender;
+    }
+    public String getCreator() {
+        return creator;
+    }
+    public int getDuration() {
+        return duration;
+    }
+    public int getYear() {
+        return year;
+    }
+    public boolean getViewed() {
+        return viewed;
+    }
+
+    //setters
+    public void setId(int id) {
+        this.id = id;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+    public void setCreator(String creator) {
+        this.creator = creator;
+    }
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+    public void setYear(int year) {
+        this.year = year;
+    }
+    public void setViewed(boolean viewed) {
+        this.viewed = viewed;
+    }
 }
